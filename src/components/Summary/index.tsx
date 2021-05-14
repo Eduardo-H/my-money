ef8289e11852104ed17c7745ac851570d4194366
Eduardo-H <edu-henrique2014@hotmail.com>
2021-05-13 23:34:20 -0300

    Initial commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,68 @@
+import { useTransactions } from '../../hooks/useTransactions';
+
+import { Container } from './styles';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+export function Summary() {
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0
+    });
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Incomes</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('us', {
+                        style: 'currency',
+                        currency: 'USD'
+                    }).format(summary.deposits)}
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Outcomes</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('us', {
+                        style: 'currency',
+                        currency: 'USD'
+                    }).format(summary.withdraws)}
+                </strong>
+            </div>
+            <div className="highlightBackground">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('us', {
+                        style: 'currency',
+                        currency: 'USD'
+                    }).format(summary.total)}
+                </strong>
+            </div>
+        </Container>
+    );
+}
